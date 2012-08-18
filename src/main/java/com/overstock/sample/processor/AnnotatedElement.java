541cfa34a33a656f8334234062adadd9253c2cd5
Ian Robertson <ian.b.robertson@gmail.com>
2012-08-18 00:05:36 -0600

    more javadoc

--- src/main/java/com/overstock/sample/processor/AnnotatedElement.java
+++ src/main/java/com/overstock/sample/processor/AnnotatedElement.java
@@ -3,6 +3,9 @@ package com.overstock.sample.processor;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 
+/**
+ * A pair consisting of an {@link Element} and an {@link AnnotationMirror} of an annotation on that element.
+ */
 class AnnotatedElement {
   public AnnotatedElement(Element element, AnnotationMirror annotationMirror) {
     this.element = element;
--- src/main/java/com/overstock/sample/processor/ElementTypePair.java
+++ src/main/java/com/overstock/sample/processor/ElementTypePair.java
@@ -3,6 +3,9 @@ package com.overstock.sample.processor;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 
+/**
+ * A pair consisting of a {@link TypeElement} and the corresponding {@link DeclaredType}
+ */
 class ElementTypePair {
   public ElementTypePair(TypeElement element, DeclaredType type) {
     this.element = element;
