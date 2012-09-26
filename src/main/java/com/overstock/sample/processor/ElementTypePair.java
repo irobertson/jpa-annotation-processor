264a5d8f39b95bfb3d09369a527f67c274f3c89c
Ian Robertson <ian.b.robertson@gmail.com>
2012-09-25 18:54:57 -0600

    Document purpose of class, as well as how its used.

--- src/main/java/com/overstock/sample/processor/ElementTypePair.java
+++ src/main/java/com/overstock/sample/processor/ElementTypePair.java
@@ -2,9 +2,14 @@ package com.overstock.sample.processor;
 
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
+import javax.lang.model.util.Types;
 
 /**
- * A pair consisting of a {@link TypeElement} and the corresponding {@link DeclaredType}
+ * A pair consisting of a {@link TypeElement} and the corresponding {@link DeclaredType}. In some cases, we need the
+ * element, while in others, we need the type. It's possible to move back and forth between these two via
+ * {@link Types#asElement(javax.lang.model.type.TypeMirror)} and
+ * {@link Types#getDeclaredType(TypeElement, javax.lang.model.type.TypeMirror...)}, but its simpler to just hang on to
+ * both.
  */
 class ElementTypePair {
   public ElementTypePair(TypeElement element, DeclaredType type) {
